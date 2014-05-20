f16a86b7b58fbd66c117fe9e295ddc70487d51ab
akshaysawant <akshaysawant@Akshays-MacBook-Pro.local>
2014-05-20 16:33:27 -0400

    Basic Hello World
    .

---
+++ View2.h
@@ -0,0 +1,18 @@
+//
+//  View2.h
+//  Hello World
+//
+//  Created by Akshay Sawant on 5/20/14.
+//  Copyright (c) 2014 Akshay Sawant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View2 : UIViewController
+{
+    IBOutlet UILabel *HelloWorldLabel;
+}
+
+-(IBAction)Button:(id)sender;
+
+@end
